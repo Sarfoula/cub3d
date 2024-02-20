a566c8e8d68e4ef7deb645998abaf464f06d8f26
tbarde-c <tbarde-c@student.42lyon.fr>
2024-02-20 12:29:24 +0100

    no more static function

--- src/parsing/skip_empty_lines.c
+++ src/parsing/skip_empty_lines.c
@@ -6,13 +6,13 @@
 /*   By: tbarde-c <tbarde-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 12:26:43 by tbarde-c          #+#    #+#             */
-/*   Updated: 2024/02/20 12:26:55 by tbarde-c         ###   ########.fr       */
+/*   Updated: 2024/02/20 12:29:13 by tbarde-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"*
 
-static char	*skip_empty_lines(int fd)
+char	*skip_empty_lines(int fd)
 {
 	char	*line;
 
